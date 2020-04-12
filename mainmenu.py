ab048e3dfc72e0c1dd4e6dfe9d74a30876a49bb4
Simon Chen <simonhchen@gmail.com>
2020-04-11 23:06:09 -0700

    add mainmenu.py

---
+++ mainmenu.py
@@ -0,0 +1,30 @@
+import cocos.menu
+import cocos.scene
+import cocos.layer
+import cocos.actions as ac
+from cocos.director import director
+from cocos.scenes.transitions import FadeTRTransition
+
+import pyglet.app
+
+from gamelayer import new_game
+
+
+class MainMenu(cocos.menu.Menu):
+    def __init__(self):
+        super(MainMenu, self).__init__('Tower Defense')
+
+        self.font_title['font_name'] = 'Oswald'
+        self.font_item['font_name'] = 'Oswald'
+        self.font_item_selected['font_name'] = 'Oswald'
+
+        self.menu_anchor_y = 'center'
+        self.menu_anchor_x = 'center'
+
+        items = list()
+        items.append(MenuItem('New Game', self.on_new_game))
+        items.append(ToggleMenuItem('Show FPS: ', self.show_fps,
+                                    director.show_FPS))
+        items.append(MenuItem('Quit', pyglet.app.exit))
+        self.create_menu(items, ac.ScaleTo(1.25, duration=0.25),
+                         ac.ScaleTo(1.0, duration=0.25))
